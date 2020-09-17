869a2a51b6571eb0a021ea4427cb4dbb467a37c6
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-09-17 10:30:48 -0400

    more asynce stuff

--- sandbox.js
+++ sandbox.js
@@ -9,14 +9,16 @@ const getTodos = (callback) => {
     if (request.readyState === 4 && request.status === 200) {
       // responseText is a json object that is returned
       // console.log(request, request.responseText);
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       // console.log('could not fetch the data');
       callback('could not fetch data', undefined);
     }
   });
 
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'data/todos.json');
   request.send();
 };
 
